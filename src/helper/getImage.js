1eca9aa7a239f67263f8cf311d00252a1707bda9
mdowns <michaeljdowns99@gmail.com>
2024-05-06 13:37:37 -0600

    Updated Images to be smaller

--- src/helper/getImage.js
+++ src/helper/getImage.js
@@ -1,21 +1,21 @@
 const getImage = (imageName) => {
   switch (imageName) {
     case "Poppin Soda":
-      return require("../assets/images/poppinSoda.png");
+      return require("../assets/images/poppinSoda.jpg");
     case "Style Stage":
-      return require("../assets/images/styleStage.png");
+      return require("../assets/images/styleStage.jpg");
     case "BYUI Rebuild":
-      return require("../assets/images/BYUIHomePage.png");
+      return require("../assets/images/BYUIHomePage.jpg");
     case "Weather App":
-      return require("../assets/images/weatherForecast.png");
+      return require("../assets/images/weatherForecast.jpg");
     case "Chess":
-      return require("../assets/images/chessPic.png");
+      return require("../assets/images/chessPic.jpg");
     case "Orbit Simulator":
-      return require("../assets/images/orbitPic.png");
+      return require("../assets/images/orbitPic.jpg");
     case "PHP Motors":
-      return require("../assets/images/phpMotorsHome.png");
+      return require("../assets/images/phpMotorsHome.jpg");
     case "C# ATM":
-      return require("../assets/images/cSharpATM.png");
+      return require("../assets/images/cSharpAtm.jpg");
     default:
       return require("../assets/images/blank.png");
   }
