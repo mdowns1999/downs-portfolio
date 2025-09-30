baf011841907671a90915da092620fd7927957f5
mdowns1999 <michael.downs@familysearch.org>
2025-09-30 16:11:55 -0600

    Test Deploy 2

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -1,58 +1,30 @@
 import classes from "./AboutPage.module.css";
-// import couplePhoto from "../../assets/images/couplePic.jpg";
-// import Paragraph from "../UI/Paragraph";
-// import Image from "../UI/Image";
+import couplePhoto from "../../assets/images/couplePic.jpg";
+import Paragraph from "../UI/Paragraph";
+import Image from "../UI/Image";
 
 const AboutPage = () => {
   return (
-  // <>
-  //   <section className={classes.aboutBox}>
-  //     <div>
-  //       <h1>Personal life</h1>
+  <>
+    <section className={classes.aboutBox}>
+      <div>
+        <h1>Personal life</h1>
 
-  //       <Paragraph>
-  //         I was born and raised in Rexburg, Idaho, and graduated from Madison
-  //         High School in 2017. After high school, I served a two-year mission
-  //         for the Church of Jesus Christ of Latter-day Saints. Upon returning, I
-  //         attended Brigham Young University – Idaho (BYU-I), where I graduated
-  //         in April 2024 with a bachelor&apos;s degree in Software Engineering.
-  //         Outside of academics, I enjoy unwinding by reading various books or
-  //         watching TV shows like Magnum PI and Hawaii 5-0. I also love spending
-  //         time with my wife and friends, especially when we go dancing—Latin,
-  //         ballroom, and country.
-  //       </Paragraph>
-  //     </div>
-  //     <Image src={couplePhoto} alt="Mike and Jami Downs" />
-  //   </section>
-  // </>
-      <div id="print-content">
-        <h1>Page Break Test</h1>
-        <div className={`page ${classes.section}`}>
-          <p>This is the first section. It should appear on the first page.</p>
-        </div>
-        <div className={classes.pageBreak} />
-        <div className={`page ${classes.section} ${classes.breakBefore}`}>
-          <p>This section should start on a new page.</p>
-        </div>
-        <div className={classes.pageBreak} />
-        <div className={`${classes.section} ${classes.breakAfter}`}>
-          <p>This section should end with a page break after it.</p>
-        </div>
-        <div className='page-break' />
-        <div className={`page ${classes.section}`}>
-          <p>
-            This section should follow the previous one, unless the break-after
-            forced a new page.
-          </p>
-        </div>
-         <div className='page-break' />
-         <div className={`page ${classes.section}`}>
-          <p>
-            This section should follow the previous one, unless the break-after
-            forced a new page.
-          </p>
-        </div>
+        <Paragraph>
+          I was born and raised in Rexburg, Idaho, and graduated from Madison
+          High School in 2017. After high school, I served a two-year mission
+          for the Church of Jesus Christ of Latter-day Saints. Upon returning, I
+          attended Brigham Young University – Idaho (BYU-I), where I graduated
+          in April 2024 with a bachelor&apos;s degree in Software Engineering.
+          Outside of academics, I enjoy unwinding by reading various books or
+          watching TV shows like Magnum PI and Hawaii 5-0. I also love spending
+          time with my wife and friends, especially when we go dancing—Latin,
+          ballroom, and country.
+        </Paragraph>
       </div>
+      <Image src={couplePhoto} alt="Mike and Jami Downs" />
+    </section>
+  </>
   );
 };
 
--- src/components/Test/TestPage.jsx
+++ src/components/Test/TestPage.jsx
@@ -1,31 +1,31 @@
 
-import styles from "./TestPage.module.css";
+import classes from "./TestPage.module.css";
 
 
 const PageBreakTest = () => {
   return (
     <div id="print-content">
       <h1>Page Break Test</h1>
-      <div className={`page ${styles.section}`}>
+      <div >
         <p>This is the first section. It should appear on the first page.</p>
       </div>
-      <div className={styles.pageBreak} />
-      <div className={`page ${styles.section} ${styles.breakBefore}`}>
+      <div />
+      <div >
         <p>This section should start on a new page.</p>
       </div>
-      <div className={styles.pageBreak} />
-      <div className={`${styles.section} ${styles.breakAfter}`}>
+      <div/>
+      <div >
         <p>This section should end with a page break after it.</p>
       </div>
-      <div className='page-break' />
-      <div className={`page ${styles.section}`}>
+      <div/>
+      <div >
         <p>
           This section should follow the previous one, unless the break-after
           forced a new page.
         </p>
       </div>
-       <div className='page-break' />
-       <div className={`page ${styles.section}`}>
+       <div />
+       <div >
         <p>
           This section should follow the previous one, unless the break-after
           forced a new page.
